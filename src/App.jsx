39b85c461fb65abb302832b339f7651a277b3a5c
Arthur Kunrath <arthur199913@hotmail.com>
2024-05-31 10:18:59 -0300

    Created initial project components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import Home from "./components/Home";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Login/Login";
+import "./App.css";
+
 const App = () => {
-  return <div>App React</div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login/*" element={<Login />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,20 @@
+import { Route, Routes } from "react-router-dom";
+import LoginForm from "./LoginForm";
+import LoginCreate from "./LoginCreate";
+import LoginPasswordLost from "./LoginPasswordLost";
+import LoginPasswordReset from "./LoginPasswordReset";
+
+const Login = () => {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<LoginForm />}></Route>
+        <Route path="/criar" element={<LoginCreate />}></Route>
+        <Route path="/perdeu" element={<LoginPasswordLost />}></Route>
+        <Route path="/resetar" element={<LoginPasswordReset />}></Route>
+      </Routes>
+    </div>
+  );
+};
+
+export default Login;
