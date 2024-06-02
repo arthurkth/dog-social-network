4d0ebfcc5fc5b36369ca1e0447b1c528d7b66c22
Arthur Kunrath <arthur199913@hotmail.com>
2024-06-02 02:03:39 -0300

    Created custom form hook

--- src/components/Forms/Input.jsx
+++ src/components/Forms/Input.jsx
@@ -1,13 +1,21 @@
 import styles from "./Input.module.css";
 
-const Input = ({ label, type, name }) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={styles.wrapper}>
       <label htmlFor={name} className={styles.label}>
         {label}
       </label>
-      <input name={name} className={styles.input} type={type} id={name} />
-      <p className={styles.error}>Error</p>
+      <input
+        name={name}
+        className={styles.input}
+        type={type}
+        id={name}
+        onChange={onChange}
+        value={value}
+        onBlur={onBlur}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
   );
 };
